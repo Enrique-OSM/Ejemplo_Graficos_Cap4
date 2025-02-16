af8fd49bea4c636f43a3e288620cd21018ecfa6d
Enrique-OSM <enriquesalazar@Air-de-Enrique.lan>
2025-02-16 15:07:51 -0600

    Uso de la clase path

--- app/src/main/java/org/example/ejemplograficos/MainActivity.java
+++ app/src/main/java/org/example/ejemplograficos/MainActivity.java
@@ -31,13 +31,20 @@ public class MainActivity extends Activity {
 
         @Override
         protected void onDraw(Canvas canvas) {
+            Path trazo = new Path();
+            trazo.addCircle(150, 150, 100, Path.Direction.CCW);
+            canvas.drawColor(Color.WHITE);
             Paint pincel = new Paint();
             pincel.setColor(Color.BLUE);
             pincel.setStrokeWidth(8);
             pincel.setStyle(Style.STROKE);
-            canvas.drawCircle(100, 100, 50, pincel);
-            pincel.setColor(getResources().getColor(R.color.color_circulo));
-            canvas.drawCircle(150, 250, 100, pincel);
+            canvas.drawPath(trazo, pincel);
+            pincel.setStrokeWidth(1);
+            pincel.setStyle(Style.FILL);
+            pincel.setTextSize(20);
+            pincel.setTypeface(Typeface.SANS_SERIF);
+            canvas.drawTextOnPath("Desarrollo de aplicaciones moviles para android", trazo, 10, 40, pincel);
+
         }
     }
 }
